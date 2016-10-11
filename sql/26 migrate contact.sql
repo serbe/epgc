e91224d90252090398e0c55951bd0deb09432960
Valeriy K <mkoctuk@gmail.com>
2016-10-11 14:55:39 +0300

    add unique to CONSTRAINT

--- sql/26 migrate contact.sql
+++ sql/26 migrate contact.sql
@@ -1,3 +1,3 @@
-ALTER TABLE contacts ADD CONSTRAINT contacts_name_birthday_key (name, birthday);
+ALTER TABLE contacts ADD CONSTRAINT contacts_name_birthday_key UNIQUE (name, birthday);
 ALTER TABLE contacts ADD COLUMN created_at TIMESTAMP without time zone;
 ALTER TABLE contacts ADD COLUMN updated_at TIMESTAMP without time zone;
